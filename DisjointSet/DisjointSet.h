43f171f5dbd8c01ffcd9d67f7f85b398d2c4cda4
Rob Rix <rob.rix@github.com>
2015-02-07 20:58:22 -0500

    Initial Commit

---
+++ DisjointSet/DisjointSet.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+/// Project version number for DisjointSet.
+extern double DisjointSetVersionNumber;
+
+/// Project version string for DisjointSet.
+extern const unsigned char DisjointSetVersionString[];
+
