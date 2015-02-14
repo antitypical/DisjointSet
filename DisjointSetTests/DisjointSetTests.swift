912da5f68fb525a87f858948c7fdd9f48df15184
Rob Rix <rob.rix@github.com>
2015-02-14 16:42:18 -0500

    Add DisjointSet.

--- DisjointSetTests/DisjointSetTests.swift
+++ DisjointSetTests/DisjointSetTests.swift
@@ -1,5 +1,26 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-import XCTest
+final class DisjointSetTests: XCTestCase {
+	let set: DisjointSet<String> = [ "a", "b", "c", "d", "e" ]
 
-final class DisjointSetTests: XCTestCase {}
+	func testEveryElementIsInitiallyDisjoint() {
+		var set = self.set
+		if !reduce(lazy(enumerate(set))
+			.map { index, _ in (index, set.find(index)) }
+			.map(==), true, { $0 && $1 }) {
+				failure("it didn't work")
+		}
+	}
+
+	func testUnionCombinesPartitions() {
+		var set = self.set
+		set.union(1, 3)
+		assertEqual(set.findAll().count, 4)
+	}
+}
+
+
+// MARK: - Imports
+
+import DisjointSet
+import XCTest
